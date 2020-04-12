21abfa598d22346c60ef2d04ff822a413dfcb8ad
Kinjal <kinjalreetoo@gmail.com>
2020-04-12 01:35:32 -0700

    Initial Commit

---
+++ flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Kinjal Reetoo on 4/12/20.
+//  Copyright © 2020 Kinjal Reetoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
