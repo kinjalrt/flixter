49d90565e49c7f37fb619b5a82b868c389cc368f
Kinjal <kinjalreetoo@gmail.com>
2020-04-19 16:10:54 -0700

    adding missing files unit 2

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Kinjal Reetoo on 4/19/20.
+//  Copyright © 2020 Kinjal Reetoo. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
